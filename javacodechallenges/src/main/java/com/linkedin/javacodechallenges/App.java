8d9a9ed263aac9e3eb85d0622a5ae213c87e93b6
Alexander Joshua <59298565+Alexjoshua14@users.noreply.github.com>
2023-01-17 20:38:30 +0000

    Finish code

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,11 +1,37 @@
 package com.linkedin.javacodechallenges;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class App {
 
     public static boolean isPasswordComplex(String password) {
-        return false;
+        // Requirements:
+        // At least six characters
+        // One uppercase letter
+        // One lowercase letter
+        // One number
+
+        // Can check for each thing separately in O(n)-time taking O(4n) == O(n)
+
+        if (password.length() < 6)
+            return false;
+
+        boolean hasDigit = false;
+        boolean hasUppercase = false;
+        boolean hasLowercase = false;
+
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+            if (Character.isDigit(c))
+                hasDigit = true;
+            else if (Character.isUpperCase(c))
+                hasUppercase = true;
+            else if (Character.isLowerCase(c))
+                hasLowercase = true;
+        }
+
+        return hasDigit && hasUppercase && hasLowercase;
     }
 
     public static void main(String[] args) {
